30e383fffbeb0b5f5694879e3423d072e5ac76d1
Ruslan Yakovlev <68195302+hellblvde@users.noreply.github.com>
2024-08-31 14:46:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    if (m < 10) {
+        m = "0" + m;
+    }
+
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    let time = `${h}:${m}:${s}`;
+    document.getElementById("clockDisplay").innerHTML = time;
+
+    setTimeout(showTime, 1000);
+}
